05b39d941fe9e86c466ef29a85790d73e940e2bb
felixvagus <42497026+felixvagus@users.noreply.github.com>
2019-04-20 10:49:31 -0700

    Add files via upload

---
+++ excercise-one.js
@@ -0,0 +1,15 @@
+const yogurtflavors = ['strawberry', 'blueberry', 'orangeberry', 'greenberry', 'cakeberry'];
+
+yorgurtflavors.push('boomberry');
+
+console.log(yogurtflavors);
+["strawberry", "blueberry", "orangeberry", "greenberry", "cakeberry", "boomberry"]
+
+const yogurtflavors = ['strawberry', 'blueberry', 'orangeberry', 'greenberry', 'cakeberry'];
+
+yogurtflavors.splice(2, 1);
+
+console.log(yogurtflavors);
+["strawberry", "blueberry", "greenberry", "cakeberry"]
+
+const yogurtflavors = ['strawberry', 'blueberry', 'orangeberry', 'greenberry', 'cakeberry'];
