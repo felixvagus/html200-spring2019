168e7167464f21c5564289845ef698a66232b89a
felixvagus <42497026+felixvagus@users.noreply.github.com>
2019-04-21 11:20:26 -0700

    Add files via upload

---
+++ excercise-three.js
@@ -0,0 +1,11 @@
+color = ['yellow', 'blue', 'green', 'red', 'purple'];
+index = 0;
+  color.forEach(function(food){
+    console.log(index.toString() + ',' + food);
+      index++;
+  });
+  = 0,yellow
+    1,blue
+    2,green
+    3,red
+    4,purple
